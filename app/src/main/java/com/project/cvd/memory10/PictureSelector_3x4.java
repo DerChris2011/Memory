73d386f162fc01b87578155bdf23b00ec081ea61
sinanpablo <35343061+sinanpablo@users.noreply.github.com>
2018-01-12 12:33:32 +0100

    first commit

--- app/src/main/java/com/project/cvd/memory10/PictureSelector_3x4.java
+++ app/src/main/java/com/project/cvd/memory10/PictureSelector_3x4.java
@@ -3,11 +3,11 @@ package com.project.cvd.memory10;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Game_6x6 extends AppCompatActivity {
+public class PictureSelector_3x4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_6x6);
+        setContentView(R.layout.activity_picture_selector_3x4);
     }
 }
